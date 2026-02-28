2e5ce228aa13abba9b456aa41b1c31d57e29566b
ysvaidya <maulivaidya534@gmail.com>
2026-02-28 15:18:45 +0530

    Final Configs.

--- NewsDetector/urls.py
+++ NewsDetector/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('news_dec_app.urls')),
+    path('', include('news_dec_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
